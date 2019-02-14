2edcfc51ba90fb913b0078b2c585bb403ddb2485
lje9117 <47608682+lje9117@users.noreply.github.com>
2019-02-14 12:52:48 -0500

    LEGO Basic Movements

---
+++ main (1).c
@@ -0,0 +1,55 @@
+#include <kipr/botball.h>
+
+void drive_forward(int distance);
+void rightturn(int degrees);
+void leftturn(int degrees);
+
+int main()
+{
+	wait_for_light(3);
+    shut_down_in(118);
+    drive_forward(450);
+    leftturn(90);
+    drive_forward(800);
+    rightturn(90);
+    drive_forward(10);
+    //end of 0-15 seconds
+    //close claw
+    rightturn(180);
+    ao();
+    return 0;
+}
+
+//int seconds or unit forward? using encoders? so void function (number of units forward or degrees)
+void drive_forward (int distance)
+       {
+	cmpc(0);
+    while(abs(gmpc(0))<distance)
+    {
+        mav(0,500);
+        mav(3,500);
+        msleep(distance/25*1000);
+    }
+       }
+
+void rightturn (int degrees)
+      {
+    cmpc(0);
+    while(abs(gmpc(0))<degrees)
+    {
+	mav(0,500);
+	mav(3,-500);
+	msleep(degrees*20);
+    }
+      }
+	
+void leftturn (int degrees)	
+      {
+    cmpc(0);
+    while(abs(gmpc(0))<degrees)
+    {
+	mav(0,-500);
+	mav(3,500);
+	msleep(degrees*20);
+    }
+      }
